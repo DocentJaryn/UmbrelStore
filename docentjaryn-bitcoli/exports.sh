e391da9bc86e890744392bd7335c720d1cf5c756
DocentJaryn <122935333+DocentJaryn@users.noreply.github.com>
2026-04-21 18:14:45 +0200

    Update exports.sh

--- docentjaryn-bitcoli/exports.sh
+++ docentjaryn-bitcoli/exports.sh
@@ -1,2 +1,5 @@
 export APP_BITCOLI_UI_PORT="27608"
 export APP_BITCOLI_API_PORT="27609"
+
+bitcoli_api_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-api/hostname"
+export APP_BITCOLI_API_HIDDEN_SERVICE="$(cat "${bitcoli_api_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")" 
