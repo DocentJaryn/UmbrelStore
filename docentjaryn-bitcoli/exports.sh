a47988bc02d67b95464c8eef1cfda6aec20fdb65
DocentJaryn <122935333+DocentJaryn@users.noreply.github.com>
2026-05-11 15:36:35 +0200

    Update exports.sh

--- docentjaryn-bitcoli/exports.sh
+++ docentjaryn-bitcoli/exports.sh
@@ -1,5 +1,6 @@
 export APP_BITCOLI_UI_PORT="27608"
 export APP_BITCOLI_API_PORT="27609"
+export APP_BITCOLI_API_IP="10.21.21.81"
 
 bitcoli_api_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-api/hostname"
 if [ -s "${bitcoli_api_hidden_service_file}" ]; then
